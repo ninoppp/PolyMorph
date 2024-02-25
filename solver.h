dff68f0bbc5d000d3d67b043c1e6869bd4ac0578
Nico <muellernico@outlook.com>
2024-02-26 00:58:36 +0100

    moved initialization to constructor. add helper function to create IC.

--- solver.h
+++ solver.h
@@ -1,12 +1,13 @@
 #include <vector>
 #include <functional>
 #include <fstream>
+#include <cmath>
 
-constexpr int N = 100; // size of the FD grid
+constexpr int N = 10; // size of the FD grid. Question: does N stay constant or does dx stay constant?
 
 struct Grid {
-    double G[N][N]; // ToDo: change datastructure to make N more flexible. 
-    double& operator()(int i, int j) { return G[i][j]; } // access element
+    double G[N][N]; // ToDo: change datastructure to std::vector
+    double& operator()(int i, int j) { return G[i][j]; }
 };
 
 struct Reaction {
@@ -25,11 +26,18 @@ struct LinearDegradation : Reaction {
 
 struct Solver {    
     Grid u; // concentration of the morphogen
-    double D; // diffusion coefficient. later also a grid
+    double D; // diffusion coefficient. Later also a grid datastructure
     double dx; // grid spacing
     // dt comes from polyhoop main program
 
-    void step(Grid &u, double dt, double dx, double D, Reaction R) { 
+    // initialize the grid with a given initial condition
+    Solver(const Grid u0, const double D = 1.0, const double dx = 0.1) {
+        this->u = u0;
+        this->D = D;
+        this->dx = dx;
+    }
+
+    void step(Grid &u, const double dt, double dx, double D, Reaction R) { 
         Grid unew;
         // Forward Euler with central differences ToDo: adapt for variable diffusion coefficient
         for (int i = 1; i < N - 1; i++) {
@@ -50,10 +58,20 @@ struct Solver {
         u = unew; 
     }
 
-    void output(int frame) {    // f: frame number
+    void output(const size_t frame) {    // f: frame number
         char filename [16]; 
         snprintf(filename, 16, "rd_frame%06zu.vts", frame);        
         std::ofstream file(filename);
         // TODO write VTK file
     }
 };
+
+// helper function for IC
+Grid create_gaussian() {
+    Grid u;
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < N; j++)
+            u(i, j) = std::exp(-((i - N/2)*(i - N/2) + (j - N/2)*(j - N/2)) / 10.0);
+    u(N/2, N/2) = 1.0;
+    return u;
+}
