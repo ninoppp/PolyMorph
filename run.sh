711c9f4d67f3308f7cce64a97e7a244a4587198d
Nico <muellernico@outlook.com>
2024-02-27 16:31:35 +0100

    exlude executable from git

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 
 #!/bin/bash
 echo "compiling polyhoop ... "
-g++ -fopenmp -O3 -o polyhoop polyhoop.cpp
+g++ -fopenmp -O3 -o polyhoop.out polyhoop.cpp
 echo "generating ensemble ... "
 perl coplanar_circles.pl 10 1 1 1 > ensemble.off # 10 vert, 1 radius, 1 circles, 1 distance 
 echo " running polyhoop ... "
-OMP_NUM_THREADS=8 ./polyhoop
+OMP_NUM_THREADS=8 ./polyhoop.out
 
 # Create a new directory for the output and move all files
 mkdir -p out/$(date +%Y-%m-%d_%H-%M)
