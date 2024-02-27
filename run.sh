e27f17735e4e68c147db755a4762e8fdb7ab794b
Nico <muellernico@outlook.com>
2024-02-27 16:23:19 +0100

    fixed shell script not saving ensemble correctly

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 
 #!/bin/bash
+echo "compiling polyhoop ... "
 g++ -fopenmp -O3 -o polyhoop polyhoop.cpp
-perl coplanar_circles.pl 10 1 1 1 # 10 vert, 1 radius, 1 circles, 1 distance 
+echo "generating ensemble ... "
+perl coplanar_circles.pl 10 1 1 1 > ensemble.off # 10 vert, 1 radius, 1 circles, 1 distance 
+echo " running polyhoop ... "
 OMP_NUM_THREADS=8 ./polyhoop
 
 # Create a new directory for the output and move all files
 mkdir -p out/$(date +%Y-%m-%d_%H-%M)
 mv *.vtp out/$(date +%Y-%m-%d_%H-%M)/
 mv *.vts out/$(date +%Y-%m-%d_%H-%M)/
+echo "all done."
