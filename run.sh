80dbc857aadc7a58dcee165394c82787cbdca73b
Nico <muellernico@outlook.com>
2024-02-28 11:10:35 +0100

    cleanup in case of aborted simulation

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 
 #!/bin/bash
+echo "cleaning up leftover output files ..."
+rm *.vtp
+rm *.vts
+
 echo "compiling polyhoop ... "
 g++ -fopenmp -O3 -o polyhoop.out polyhoop.cpp
 echo "generating ensemble ... "
 perl coplanar_circles.pl 10 1 1 1 > ensemble.off # 10 vert, 1 radius, 1 circles, 1 distance 
+
 echo " running polyhoop ... "
 OMP_NUM_THREADS=8 ./polyhoop.out
 
