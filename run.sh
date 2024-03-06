d154c48188fced54b769bec3475519efe1fe0628
Nico <muellernico@outlook.com>
2024-03-06 17:13:32 +0100

    cleaner shell script with options for testing and cleanup

--- run.sh
+++ run.sh
@@ -1,19 +1,54 @@
 
 #!/bin/bash
-echo "cleaning up leftover output files ..."
-rm *.vtp
-rm *.vts
 
-echo "compiling polyhoop ... "
-g++ -fopenmp -O3 -o polyhoop.out polyhoop.cpp
-echo "generating ensemble ... "
-perl coplanar_circles.pl 10 1 1 1 > ensemble.off # 10 vert, 1 radius, 1 circles, 1 distance 
+cleanup_files() {
+    echo "cleaning up leftover output files ..."
+    rm *.vtp
+    rm *.vts
+}
 
-echo " running polyhoop ... "
-OMP_NUM_THREADS=8 ./polyhoop.out
+compile_polyhoop() {
+    echo "compiling polyhoop ... "
+    g++ -fopenmp -O3 -o polyhoop.out polyhoop.cpp
+}
 
-# Create a new directory for the output and move all files
-mkdir -p out/$(date +%Y-%m-%d_%H-%M)
-mv *.vtp out/$(date +%Y-%m-%d_%H-%M)/
-mv *.vts out/$(date +%Y-%m-%d_%H-%M)/
+generate_ensemble() {
+    echo "generating ensemble ... "
+    perl coplanar_circles.pl 10 1 1 1 > ensemble.off # 10 vert, 1 radius, 1 circles, 1 distance 
+}
+
+run_polyhoop() {
+    echo "running polyhoop ... "
+    OMP_NUM_THREADS=8 ./polyhoop.out
+}
+
+move_files() {
+    echo "moving output files to desktop ..."
+    local timestamp=$(date +%Y-%m-%d_%H-%M)
+    local path="/mnt/c/Users/muell/Desktop/PolymorphOutput" # change this to your desktop path
+    mkdir $path/$timestamp
+    mv *.vtp $path/$timestamp/
+    mv *.vts $path/$timestamp/ 
+}
+
+run_test() {
+    g++ test/unit_tests.cpp -o test/a.out
+    ./test/a.out
+}
+
+if [ "$1" = "c" ]; then
+    echo "running cleanup only ..."
+    cleanup_files
+elif [ "$1" = "t" ]; then
+    echo "running test only ..."
+    run_test
+    move_files
+else
+    echo "running full pipeline ..."
+    compile_polyhoop
+    generate_ensemble
+    run_polyhoop
+    move_files
+fi
+
 echo "all done."
