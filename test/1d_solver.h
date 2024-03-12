809821e7c865f07f500a448739253fa4125c5c96
Nico <muellernico@outlook.com>
2024-03-12 20:23:27 +0100

    attempt at solving 1D case only, but still no good enough approximation to analytical solution

--- test/1d_solver.h
+++ test/1d_solver.h
@@ -1,7 +1,20 @@
 #include <vector>
-#include "../solver.h"
 
-struct Solver {  
+struct Reaction {
+    double operator()(double u, int i, int j) {
+        return 0.0; // ToDo: implement reaction function
+    }
+};
+
+struct LinearDegradation : Reaction {
+    double k;
+    LinearDegradation(double k): k(k) {}
+    double operator()(double u, int i, int j) {
+        return -k * u;
+    }
+};
+
+struct Solver1D {  
     std::vector<double> u; // concentration of the morphogen
     double D; // diffusion coefficient. Later also a grid datastructure
     double dx; // grid spacing
@@ -9,7 +22,7 @@ struct Solver {
     Reaction R; // reaction term
 
     // initialize the grid with a given initial condition
-    Solver(std::vector<double> u0, const double D = 1.0, const double dx = 0.1, 
+    Solver1D(std::vector<double> u0, const double D = 1.0, const double dx = 0.1, 
             double dt = 1e-4, Reaction R = LinearDegradation(0.1)) {
         this->u = u0;
         this->D = D;
@@ -19,23 +32,18 @@ struct Solver {
     }
 
     void step() { 
-        std::vector<double> unew;
+        std::vector<double> unew (u.size(), 0.0);
         // Forward Euler with central differences ToDo: adapt for variable diffusion coefficient
         // maybe separate inner nodes and boundary to efficiently parallelize and vectorize inner nodes
-        #pragma omp parallel for collapse(2)
-        for (int i = 0; i < Nx; i++) {
-            for (int j = 0; j < Ny; j++) {   
-                // mirror past-boundary nodes
-                double n = (j == Ny-1) ? u(i, j-1) : u(i, j+1);
-                double s = (j == 0)    ? 1         : u(i, j-1); // dirichlet boundary condition
-                double e = (i == Nx-1) ? u(i-1, j) : u(i+1, j);
-                double w = (i == 0)    ? u(i+1, j) : u(i-1, j);
-                unew(i, j) = u(i, j) + dt * (
-                    D / (dx*dx) * (n + s + e + w - 4 * u(i, j))
-                    + R(u(i, j), i, j)
-                ); 
-            }
-        }      
+        #pragma omp parallel for
+        for (int i = 1; i < u.size() - 1; i++) {
+            unew[i] = u[i] + dt * (
+                D / (dx*dx) * (u[i+1] + u[i-1] - 2 * u[i])
+                + R(u[i], 0, 0)
+            ); 
+        }
+        unew[0] = 1;
+        unew[u.size() - 1] = unew[u.size() - 2]; // consider last cell as ghost cell
         u = unew; 
     }
 };
--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -1,15 +1,16 @@
-#include "../solver.h"
 #include <iostream>
 #include <iomanip> // Add this line
 #include <fstream>
 
+//#include "../solver.h"
+#include "1d_solver.h"
 // #include <gtest/gtest.h>
 
 void print(std::string s) {
     std::cout << s << std::endl;
 }
-
-void print_grid(Grid u, int N) {
+/*/
+void print_grid(Grid<double> u, int N) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             std::cout << std::setw(4) << std::fixed << std::setprecision(4) << u(i, j) << " ";            
@@ -21,7 +22,7 @@ void print_grid(Grid u, int N) {
 
 // only run solver same as in polyhoop
 void run_solver() {
-    Grid u;
+    Grid<double> u;
     Solver solver(u, 0.03, 0.01, 1e-4, LinearDegradation(0.01));
     for (int f = 0; f < 100; f++) {
         for (int s = 0; s < 3000; s++) {
@@ -36,10 +37,31 @@ void run_solver() {
         file << solver.u(1,j) << std::endl;
     }
     file.close();
+}*/
+
+void run_1d_solver() {
+    std::cout << "running 1d solver" << std::endl;
+    int n = 2000;
+    std::vector<double> u0(n, 0.0);
+    u0[0] = 1.0; // dirichlet boundary condition
+    Solver1D solver(u0, 0.03, 0.01, 1e-5, LinearDegradation(0.01));
+    for (int f = 0; f < 100; f++) {
+        for (int s = 0; s < 300000; s++) {
+            solver.step();
+        }
+        //solver.output(f);
+        std::cout << "frame " << f << std::endl;
+    }
+    // final (hopefully steady) state
+    std::ofstream file("test/final_frame.txt");
+    for (int i = 0; i < n; i++) {
+        file << solver.u[i] << std::endl;
+    }
+    file.close();
 }
     
 int main() {    
-    run_solver();
+    run_1d_solver();
     return 0;
 }
 
