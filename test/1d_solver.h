dc66a2893aa53e9318bb177edd870986ca45c9b9
Nico <muellernico@outlook.com>
2024-03-21 13:44:23 +0100

    rename diffusion coefficient

--- test/1d_solver.h
+++ test/1d_solver.h
@@ -17,17 +17,17 @@ struct LinearDegradation : Reaction {
 
 struct Solver1D {  
     std::vector<double> u; // concentration of the morphogen
-    double D; // diffusion coefficient. Later also a grid datastructure
+    double D0; // diffusion coefficient. Later also a grid datastructure
     double dx; // grid spacing
     double dt; // time step
     Reaction R; // reaction term
     double norm = 0; // keep track of change in u
 
     // initialize the grid with a given initial condition
-    Solver1D(std::vector<double> u0, const double D = 1.0, const double dx = 0.1, 
+    Solver1D(std::vector<double> u0, const double D0 = 1.0, const double dx = 0.1, 
             double dt = 1e-4, Reaction R = LinearDegradation(0.1)) {
         this->u = u0;
-        this->D = D;
+        this->D0 = D0;
         this->dx = dx;
         this->dt = dt;
         this->R = R;
@@ -41,7 +41,7 @@ struct Solver1D {
         #pragma omp parallel for
         for (int i = 1; i < u.size() - 1; i++) {
             unew[i] = u[i] + dt * (
-                D / (dx*dx) * (u[i+1] + u[i-1] - 2 * u[i])
+                D0 / (dx*dx) * (u[i+1] + u[i-1] - 2 * u[i])
                 + R(u[i], 0, 0)
             ); 
         }
