bb786812d8f1b2ace8ba17ba2784dc621b7ada1d
Nico <muellernico@outlook.com>
2024-04-11 14:07:16 +0200

    reproduce double neumann steady state. some cleanup

--- test/plot.py
+++ test/plot.py
@@ -3,15 +3,15 @@ import math
 
 c0 = 1
 D = 0.03
-j = 0.01 # influx
+j = 0.03 # influx
 dx = 0.01
-k = 0.01
-L = 100.0
+k = 0.05
+L = 1.0
 lam = 1.0
 if k != 0:
     lam = math.sqrt(D / k)
 
-# analytical solution of standard model
+# analytical solution of standard model u(x->inf) = 0
 def u_standard(x):
     return c0 * math.exp(-x/lam)
 
@@ -22,38 +22,27 @@ def u_linear(x):
 # solution of double neumann
 def u_2xneumann(x):
     mu = 1/lam
-    return j/mu * (math.exp(mu * (-2 * L + x)) + math.exp(-mu * x)) / (1 - math.exp(2 * mu))
+    return j/mu * (math.exp(mu * (2 - x)) + math.exp(mu * x)) / (math.exp(2 * mu) - 1)
 
 # - select the analytical solution - #
-u = u_standard
+u = u_2xneumann
 
 # Read the values from final_frame.txt
 with open('test/final_frame.txt', 'r') as file:
     values = [float(line.strip()) for line in file]
-    values = values[0:int(len(values)/5)]
+    values = values[0:int(len(values))]
 
-# Generate x values for the analytical solution
 x_values = [i * dx for i in range(len(values))]
-
-# Calculate the analytical solution for each x value
 analytical_values = [u(x) for x in x_values]
-
 difference = [abs(v - a) for v, a in zip(values, analytical_values)]
-print(values[0], analytical_values[0], difference[0])
 
-# Plot the analytical solution
-#plt.plot(x_values, analytical_values, ':', label='Analytical Solution')
-# Plot the polymorph solution
-#plt.plot(x_values, values, ':', label='Numerical Solution')
-# Plot the difference
-plt.plot(x_values[::10], difference[::10], 'o', ms=3, label='absolute numerical error |u - u_analytical|')
+plt.plot(x_values, analytical_values, ':', label='Analytical Solution')
+plt.plot(x_values, values, ':', label='Numerical Solution')
+#plt.plot(x_values[::1], difference[::1], 'o', ms=3, label='absolute numerical error |u - u_analytical|')
 
-#plt.ylabel('concentration u')
-#plt.ylim(min(difference), max(difference))
-plt.yscale('log')
 plt.xlabel('x')
+plt.ylabel('concentration u')
+#plt.yscale('log')
 
-# Set the legend
 plt.legend()
-# Save the plot as a PDF file
 plt.savefig('/home/uranus/PolyMorph/test/solution_plot.pdf')
--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
-#include <iomanip> // Add this line
+#include <iomanip>
 #include <fstream>
 #include <cmath>
 
 //#include "../solver.h"
 #include "1d_solver.h"
-// #include <gtest/gtest.h>
 
 constexpr double c0 = 1;
 constexpr double D = 0.03;
-constexpr double j = 0.01; //influx, negative gradient at left boundary
+constexpr double j = 0.03; //influx, negative gradient at left boundary
 constexpr double dx = 0.01;
-constexpr double k = 0.01;
-constexpr double dt = 1e-3;
-constexpr double L = 100.0;
+constexpr double k = 0.05;
+constexpr double dt = 1e-4;
+constexpr double L = 1.0;
 constexpr int N = L/dx;
 
-constexpr unsigned Ns = 10000;
+constexpr unsigned Ns = 500000;
 constexpr unsigned Nf = 100;
 
 const double lambda = std::sqrt(D/k);
-const double tau = (1+L/10/lambda ) / (2*k); // time to stead state from Roman
+const double tau_standard = (1+L/10/lambda ) / (2*k); // time to stead state from Roman
 //constexpr double tau = L*L / (D * N*N * M_PI*M_PI); // from script for double dirichlet
+const double tau_neumann = L*L / (D*M_PI*M_PI + k*L*L); // not working somehow
+const double tau = tau_standard;
 
 void print(std::string s) {
     std::cout << s << std::endl;
@@ -60,12 +61,11 @@ void run_1d_solver() {
     std::cout << "running 1d solver" << std::endl;
     std::vector<double> u0(N, 0.0);
     u0[0] = 1.0; // dirichlet boundary condition
-    Solver1D solver(u0, D, dx, dt, k);
+    Solver1D solver(u0, D, dx, dt, k, "2xNeumann", j);
     for (int f = 0; f < Nf; f++) {
         for (int s = 0; s < Ns; s++) {
             solver.step();
         }
-        //solver.output(f);
         double time = f*Ns*dt;
         std::cout << 
             "frame " << f << 
