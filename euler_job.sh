2fe241194a6a8af3b7680dbbea47231cae1c731e
Nico Müller <muellernico@outlook.com>
2024-04-17 15:38:51 +0200

    running with 128 threads

--- euler_job.sh
+++ euler_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=Polymorph-%j.err  # Error file  (default: slurm-%j.out)
 #SBATCH --ntasks=1                # Number of tasks
 #SBATCH --constraint=EPYC_7763    # Select node with CPU
-#SBATCH --cpus-per-task=8        # Number of CPUs per task
+#SBATCH --cpus-per-task=128        # Number of CPUs per task
 #SBATCH --mem-per-cpu=1024        # Memory per CPU
 #SBATCH --time=00:20:00           # Wall clock time limit
 
@@ -13,7 +13,7 @@ module list
 
 echo "compiling polyhoop ... "
 g++ -fopenmp -O3 -o polyhoop.out polyhoop.cpp
-OMP_NUM_THREADS=8
+OMP_NUM_THREADS=128
 export OMP_NUM_THREADS
 echo "running polyhoop in parallel with $OMP_NUM_THREADS threads... "
 ./polyhoop.out
