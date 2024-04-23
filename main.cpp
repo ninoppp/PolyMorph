857272bb5bf2b9453e22218dac90795355f668d0
Nico <muellernico@outlook.com>
2024-04-23 17:41:52 +0200

    setup border sharpness experiment. writing config to output

--- main.cpp
+++ main.cpp
@@ -5,18 +5,26 @@
 #include "chemistry.h"
 #include <iostream>
 
-void sharpness_experiment() { // TODO: modify distributions
-  double CV[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 1.0};
+void sharpness_experiment() {
+  double CV[] = {0.5, 0.7, 1.0};
   std::ofstream file("sharpness.txt");
   for (double cv : CV) {
+    // adjust distributions
+    double lnCV = std::log(1 + cv*cv);
+    D_dist = std::lognormal_distribution<double>(std::log(D_mu) - lnCV/2, std::sqrt(lnCV));
+    k_dist = std::lognormal_distribution<double>(std::log(k_mu) - lnCV/2, std::sqrt(lnCV));
+    p_dist = std::lognormal_distribution<double>(std::log(p_mu) - lnCV/2, std::sqrt(lnCV));
+    // ToDo: independent
+    threshold_dist = std::lognormal_distribution<double>(std::log(threshold_mu) - lnCV/2, std::sqrt(lnCV));
+
     Ensemble ensemble("ensemble/rect_100x50.off");
     Chemistry chemistry(ensemble);
-    Grid<double> u0(int(100/dx), int(50/dx)); // initial condition, just zeros
+    Grid<double> u0(int(102/dx), int(52/dx)); // initial condition, just zeros
     Solver solver(u0, D0, dx, dt, k0); // init solver
     Interpolator interpolator(ensemble, solver);
     chemistry.is_producing = [solver](const Polygon& p) { return p.midpoint().x < solver.box_position_x + 10; }; // heavyside
-    ensemble.output(0); // print the initial state
-    solver.output(0); // print the initial state
+    ensemble.output(cv*10000); // print the initial state
+    solver.output(cv*10000); // print the initial state
     ensemble.step(); // update boxes and everything
     chemistry.update();
     interpolator.scatter(); 
@@ -26,13 +34,24 @@ void sharpness_experiment() { // TODO: modify distributions
       {
         solver.step();
       } 
+      interpolator.gather(); // get u
+      chemistry.update(); // flag
+      ensemble.output(int(cv*10000 + f)); // print a frame
+      solver.output(int(cv*10000 + f)); // print a frame
     }
-    interpolator.gather(); // get u
-    chemistry.update(); // flag
-    ensemble.output(int(cv*100)); // print a frame
-    solver.output(int(cv*100)); // print a frame
     file << "CV=" << cv << " sharpness=" << chemistry.get_border_sharpness() << std::endl;
   }
+  file << "config:" << std::endl
+      << "D0=" << D0 << std::endl
+      << "D_mu=" << D_mu << std::endl
+      << "k_mu=" << k_mu << std::endl
+      << "p_mu=" << p_mu << std::endl
+      << "threshold_mu=" << threshold_mu << std::endl
+      << "dx=" << dx << std::endl
+      << "dt=" << dt << std::endl
+      << "Nf=" << Nf << std::endl
+      << "Ns=" << Ns << std::endl;
+      
   file.close();
 }
 
