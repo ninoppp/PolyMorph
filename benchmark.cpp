01f5862fd22f3fdc6f5f3b775f9a6921da63b895
Nico <muellernico@outlook.com>
2024-04-25 01:02:05 +0200

    benchmarking solver with larger grids

--- benchmark.cpp
+++ benchmark.cpp
@@ -10,33 +10,37 @@
 int main() {
     std::cout << "Running benchmark" << std::endl;
     rng.seed(90178009);
-    Ensemble ensemble("ensemble/box_100x50.off"); // read the input file
+
+    Ensemble ensemble("ensemble/default.off"); // read the input file
     
-    size_t N = 256;
-    size_t frames = 100;
-    size_t timesteps = 2000;
+    size_t frames = 10;
+    size_t timesteps = 1000;
 
-    Grid<double> u0(N, N); // initial condition, just zeros
-    Solver solver(u0, D0, dx, dt, k0); // init solver
-    Interpolator interpolator(ensemble, solver);
-    Chemistry chemistry(ensemble);
-    chemistry.is_producing = [solver](const Polygon& p) { return p.midpoint().x < solver.box_position_x + 10; }; // heavyside
-
-    std::ofstream file("benchmark_ensemble_box.txt", std::ios::app);
+    std::ofstream file("benchmark_solver.csv", std::ios::app);
     file << "num_threads frame num_gridpoints num_vertices time" << std::endl;
 
     int num_threads = omp_get_max_threads();
-    for (std::size_t f = 1; f <= frames; ++f) {
-        double start = walltime();
-        for (std::size_t s = 0; s < timesteps; ++s) {
-            ensemble.step();
-        } 
-        double end = walltime();
-        size_t num_vertices = 0;
-        for (const Polygon& p : ensemble.polygons) {
-            num_vertices += p.vertices.size();
+    
+    for (int N = 2048; N <= 8192; N *= 2) {
+
+        Grid<double> u0(N, N); // initial condition, just zeros
+        Solver solver(u0, D0, dx, dt, k0); // init solver
+        Interpolator interpolator(ensemble, solver);
+        Chemistry chemistry(ensemble);
+        chemistry.is_producing = [solver](const Polygon& p) { return p.midpoint().x < solver.box_position_x + 10; }; // heavyside
+
+        for (std::size_t f = 1; f <= frames; ++f) {
+            double start = walltime();
+            for (std::size_t s = 0; s < timesteps; ++s) {
+                solver.step();
+            } 
+            double end = walltime();
+            size_t num_vertices = 0;
+            for (const Polygon& p : ensemble.polygons) {
+                num_vertices += p.vertices.size();
+            }
+            file << num_threads << " " << f << " " << N*N << " " << num_vertices << " " << end - start << std::endl;
         }
-        file << num_threads << " " << f << " " << N*N << " " << num_vertices << " " << end - start << std::endl;
     }
     file.close();
     return 0;
