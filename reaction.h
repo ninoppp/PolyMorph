077b82c2ecfa61cf17ae7d237864b0b8112c2f08
Nico <muellernico@outlook.com>
2024-04-27 23:09:27 +0200

    adapted reactions for multi species. added inhibition reaction.

--- reaction.h
+++ reaction.h
@@ -1,19 +1,31 @@
 #include <vector>
 #include "const.h"
 
+
 struct Reaction {
-    virtual std::vector<double> operator()(std::vector<double> u, std::vector<double> k) {
-        std::vector<double> r(num_species);
+    std::vector<double> operator()(std::vector<double> u, std::vector<double> k) {
+        std::vector<double> r(NUM_SPECIES);
         return r;
     }
 };
 
 struct LinearDegradation : Reaction {
     std::vector<double> operator()(std::vector<double> u, std::vector<double> k) {
-        std::vector<double> r(num_species);
-        for (int i = 0; i < num_species; i++) {
+        std::vector<double> r(NUM_SPECIES);
+        for (int i = 0; i < NUM_SPECIES; i++) {
             r[i] = -k[i]*u[i];
         }
+        return r;
+    }
+};
+
+struct Inhibition : Reaction {
+    std::vector<double> operator()(std::vector<double> u, std::vector<double> k) {
+        std::vector<double> r(NUM_SPECIES);
+        r = {
+            -k[0]*u[0] - k[2]*u[0]*u[1],
+            -k[1]*u[1] - k[2]*u[0]*u[1]
+        };
         return r;
     }
 };
