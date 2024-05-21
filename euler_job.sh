6262570c53b075bb0048a171cc7cd9e3cfd10b01
Nico <muellernico@outlook.com>
2024-05-21 14:37:28 +0200

    adjusted run scripts. more cores on euler. folder for move in run.sh

--- euler_job.sh
+++ euler_job.sh
@@ -4,8 +4,8 @@
 #SBATCH --error=Polymorph-%j.err  # Error file  (default: slurm-%j.out)
 #SBATCH --ntasks=1                # Number of tasks
 #SBATCH --constraint=EPYC_7763    # Select node with CPU
-#SBATCH --cpus-per-task=4         # Number of CPUs per task
-#SBATCH --mem-per-cpu=2048        # Memory per CPU
+#SBATCH --cpus-per-task=32         # Number of CPUs per task
+#SBATCH --mem-per-cpu=1024        # Memory per CPU
 #SBATCH --time=08:00:00           # Wall clock time limit
 
 module load gcc
@@ -14,7 +14,7 @@ module list
 echo "compiling polymorph ... "
 rm -f polymorph.out
 g++ -fopenmp -O3 -o polymorph.out main.cpp
-OMP_NUM_THREADS=4
+OMP_NUM_THREADS=32
 export OMP_NUM_THREADS
 echo "running polymorph in parallel with $OMP_NUM_THREADS threads... "
 ./polymorph.out
