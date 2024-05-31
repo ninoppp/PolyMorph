eec3de19daf8fefd02d6ad178554748137da898e
Nico Müller <muellernico@outlook.com>
2024-05-31 16:59:31 +0200

    improved and simplified shell script

--- run.sh
+++ run.sh
@@ -1,70 +1,42 @@
-
 #!/bin/bash
 
+#output="./out"
+output="/mnt/c/Users/nico/Desktop/PolymorphOutput" # nico or muell
+OMP_NUM_THREADS=8
+
 move_files() {
-    echo "moving output files to desktop ..."
+    echo "moving output files to folder ..."
     local timestamp=$(date +%Y-%m-%d_%H-%M)
-    local path="/mnt/c/Users/muell/Desktop/PolymorphOutput" # change this to your desktop path (nico or muell)
-    mkdir $path/$timestamp
-    mv *.vtp $path/$timestamp/
-    mv *.vts $path/$timestamp/ 
-    mv *.cfg $path/$timestamp/
-    mv *.pvd $path/$timestamp/
+    mkdir $output/$timestamp
+    mv *.vtp *.vts *.cfg "$output/$timestamp/"
 }
 
 cleanup_files() {
     echo "cleaning up leftover output files ..."
-    rm *.vtp
-    rm *.vts
-    rm *.cfg
-    rm *.pvd
+    rm *.vtp *.vts *.cfg
 }
 
-compile_polyhoop() {
-    echo "compiling polymorph ... "
-    rm -f polymorph.out
-    g++ -fopenmp -O3 -o polymorph.out src/main.cpp
-}
-
-generate_ensemble() {
-    echo "generating ensemble ... "
-    perl coplanar_circles.pl 10 1 1 1 > ensemble.off # 10 vert, 1 radius, 1 circles, 1 distance 
-}
-
-run_polyhoop() {
-    OMP_NUM_THREADS=8
+run() {
+    echo "compiling src/$1.cpp ..."
+    rm -f polymorph
+    g++ -fopenmp -O3 -o polymorph "src/$1.cpp"
     export OMP_NUM_THREADS
-    echo "running polymorph with $OMP_NUM_THREADS threads... "
-    ./polymorph.out
-}
-
-compile_and_run_test() {
-    g++ -fopenmp -O3 -o test/a.out test/unit_tests.cpp 
-    ./test/a.out
-}
-
-plot_steady_state() {
-    echo "plotting steady state ..."
-    python3 test/plot.py
+    echo "running $1 with $OMP_NUM_THREADS threads... "
+    ./polymorph
 }
 
 if [ "$1" = "c" ]; then
-    echo "running cleanup only ..."
     cleanup_files
-
-elif [ "$1" = "t" ]; then
-    echo "running test only ..."
-    compile_and_run_test
-    plot_steady_state
     
 elif [ "$1" = "m" ]; then
     move_files
 
+elif [ -n "$1" ]; then
+    run "$1"
+    move_files
+
 else
-    echo "running full pipeline ..."
-    compile_polyhoop
-    #generate_ensemble
-    run_polyhoop
+    run "main"
     move_files
 fi
 
