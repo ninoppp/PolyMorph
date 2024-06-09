96aacceafbbda8b44586a3e6c9352fd791756fad
Nico Müller <muellernico@outlook.com>
2024-06-09 15:53:10 +0200

    started cpp for convergence study

--- include/experiments.h
+++ include/experiments.h
@@ -9,36 +9,7 @@
 #include "interpolator.h"
 #include "reaction.h"
 
-/*! \brief This file contains multiple "main" functions for different experiments
- *
- *  The main routines are called from the main() function in main.cpp
- *  and contain the logic for different simulation runs.
- */
-
-void default_testrun() {
-    double L = 40;
-    Domain domain(-L/2, -L/2, L/2, L/2);
-    Ensemble ensemble("ensemble/default.off", domain); 
-    Solver solver(domain, dx, Reactions::linearDegradation); // init solver
-    //solver.boundary.west = {BoundaryCondition::Type::Dirichlet, 0};
-
-    Interpolator interpolator(ensemble, solver);
-    ensemble.is_producing = [](const Polygon& p) { return std::vector<bool> {p.vertices[0].p == Nr}; };
-
-    ensemble.output(0); // print the initial state
-    solver.output(0); // print the initial state
-    for (std::size_t f = 1; f <= Nf; ++f) {
-        for (std::size_t s = 0; s < Ns; ++s) {
-            ensemble.step(); 
-            interpolator.scatter();
-            solver.step(dt);
-            interpolator.gather();
-            domain.step(dt); // only needed if domain is growing/shrinking
-        } 
-        ensemble.output(f);
-        solver.output(f);
-    }
-}
+// TODO move relevant experiments to own files and delete this one
 
 void chemotaxis_experiment() {
     assert(NUM_SPECIES == 1 && "Chemotaxis assumes one species");
--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,32 @@
 #include "ensembleController.h"
 #include "utils.h"
 
+void default_testrun() {
+    double L = 40;
+    Domain domain(-L/2, -L/2, L/2, L/2);
+    Ensemble ensemble("ensemble/default.off", domain); 
+    Solver solver(domain, dx, Reactions::linearDegradation); // init solver
+    //solver.boundary.west = {BoundaryCondition::Type::Dirichlet, 0};
+    Interpolator interpolator(ensemble, solver);
+    ensemble.is_producing = [](const Polygon& p) { return std::vector<bool> {p.vertices[0].p == Nr}; };
+    ensemble.output(0); // print the initial state
+    solver.output(0); // print the initial state
+    for (std::size_t f = 1; f <= Nf; ++f) {
+        for (std::size_t s = 0; s < Ns; ++s) {
+            ensemble.step(); 
+            interpolator.scatter();
+            solver.step(dt);
+            interpolator.gather();
+            domain.step(dt); // only needed if domain is growing/shrinking
+        } 
+        ensemble.output(f);
+        solver.output(f);
+    }
+}
+
 int main(int argc, char* argv[]) {
   welcome();
   validate_parameters();
   write_config();
-  //default_testrun();
-  const char* nodeID_str = getenv("SLURM_NODEID");
-  if (!nodeID_str) {
-      std::cerr << "SLURM_NODEID is not set." << std::endl;
-      return 1;
-  }
-  int nodeID = std::atoi(nodeID_str);
-  std::cout << "Node ID: " << nodeID << std::endl;
+  default_testrun();
 }
-
