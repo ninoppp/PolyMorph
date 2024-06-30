86a11e1f9143b9949f9d18b11275ba5d7a3f3d2d
Nico Müller <muellernico@outlook.com>
2024-06-30 22:55:40 +0200

    changed colors, axis label and title of animation

--- bench/convergence/animate_solution.py
+++ bench/convergence/animate_solution.py
@@ -2,19 +2,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib import colormaps
 
 data = pd.read_csv('solution.csv')
 length = 50 # interesting part of domain
+viridis = colormaps.get_cmap('viridis')
+colors = viridis(np.linspace(0, 1, 2))
 
 fig, ax = plt.subplots()
-line1, = ax.plot([], [], 'r-', label='Analytic')  # Red line for analytic solution
-line2, = ax.plot([], [], 'b-', label='Simulated')  # Blue line for simulated solution
+line1, = ax.plot([], [], color=colors[0], label='Analytic solution')  # Red line for analytic solution
+line2, = ax.plot([], [], color=colors[1], label='Simulated solution')  # Blue line for simulated solution
 ax.set_xlim(data['x'].min(), length)  # Set x-axis limits
 ax.set_ylim(min(data['analytic'].min(), data['simulated'].min()), max(data['analytic'].max(), data['simulated'].max()))  # Set y-axis limits
 ax.legend()
-ax.set_xlabel('Position (x)')
-ax.set_ylabel('Solution Value')
-ax.set_title('Convergence of Analytic and Simulated Solutions (dx=0.3)')
+ax.set_xlabel('Position x')
+ax.set_ylabel('Concentration c(x)')
+ax.set_title('Convergence of numerical solution (dx=0.3, dt=1e-4)')
 
 def update(frame):
     df = data[data['t'] == frame]
