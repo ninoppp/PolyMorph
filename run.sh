f9858d243f89352a54570c9f49035d0c0ddc8c17
Nico Müller <muellernico@outlook.com>
2024-07-30 20:05:54 +0200

    changed shell script for other users, switch to out/ folder.

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#output="./out"
-output="/mnt/c/Users/nico/Desktop/PolymorphOutput" # nico or muell
-OMP_NUM_THREADS=8
+OMP_NUM_THREADS=8 # <- change this to available cores
+output="./out" # <- change this to your desired output folder
+#output="/mnt/c/Users/nico/Desktop/PolymorphOutput"
 
 move_files() {
     echo "moving output files to folder ..."
