75cbc583ee2f905c17cbaf9278aff5b3737bf6bc
Nico Müller <muellernico@outlook.com>
2024-08-03 22:05:30 +0200

    made polygons argument of is_producing and set_flag const reference (const correctness). guarding production lambda size check with debug mode

--- include/ensemble.h
+++ include/ensemble.h
@@ -35,9 +35,9 @@ struct Ensemble
   std::vector<std::lognormal_distribution<>> p_dist = create_lognormal(p_mu, p_CV);
   std::vector<std::lognormal_distribution<>> threshold_dist = create_lognormal(threshold_mu, threshold_CV);
 
-  // lambda functions
-  std::function<std::vector<bool>(Polygon&)> is_producing = [](Polygon& p) { return std::vector(NUM_SPECIES, false); };
-  std::function<int(Polygon&)> set_flag = [](Polygon& p) { return p.u[0] < p.threshold[0]; };
+  // lambda functions ToDo: make const reference? should not create problem I think
+  std::function<std::vector<bool>(const Polygon&)> is_producing = [](const Polygon& p) { return std::vector(NUM_SPECIES, false); };
+  std::function<int(const Polygon&)> set_flag = [](const Polygon& p) { return p.u[0] < p.threshold[0]; };
 
   Ensemble(const char* name, Domain& domain, int seed=RNG_SEED) : t(0), domain(domain)
   {
@@ -401,10 +401,12 @@ struct Ensemble
       // PolyMorph extension: set production
       const std::vector<bool> producing = is_producing(polygons[p]); // which species are produced by the cell
       const std::vector<double> p_sample = sample(p_dist, rng);
+      #if DEBUG
       if (producing.size() != NUM_SPECIES) {
         std::cerr << "is_producing function must return a vector of size NUM_SPECIES" << std::endl;
         exit(1);
       }
+      #endif
       for (int i = 0; i < NUM_SPECIES; i++) {
         if (polygons[p].p[i] == 0 && producing[i]) {
           polygons[p].p[i] = p_sample[i];
